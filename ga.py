9fd06a95eb862fb084cb509af20a39b1a299a2b8
liminat <180078053+liminat@users.noreply.github.com>
2023-10-13 07:07:33 +0000

    removed deprecated functions

--- ga.py
+++ ga.py
@@ -1,4 +1,5 @@
 # This file uses genetic algorithm to optimaize the parameters of 
 # matched filter response.
 
-# In this script, we use DEAP to implement genetic a
+# In this script, we use DEAP to implement genetic algorithm. You 
+# c
