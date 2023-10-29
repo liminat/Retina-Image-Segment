c50c339a40cc8cb234ac4101cffa4aebdaf9066a
liminat <180078053+liminat@users.noreply.github.com>
2023-10-29 21:22:20 +0000

    Update opam

--- ga.py
+++ ga.py
@@ -29,4 +29,4 @@ gt = cv2.cvtColor(gt, cv2.COLOR_BGR2GRAY)
 im1 = im1.astype(np.float32) / 255.0
 gt = gt.astype(np.float32) / 255.0
 
-# create two variables of indi
+# create two variables of individual and fitnes
