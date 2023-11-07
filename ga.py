ad9109c1ff78760e2f77c55d27136103d39cd90c
liminat <180078053+liminat@users.noreply.github.com>
2023-11-07 18:47:15 +0000

    amorale-LP-276_measurement_based_d_term-8b4a1d63-8c860591

--- ga.py
+++ ga.py
@@ -39,4 +39,5 @@ toolbox = base.Toolbox()
 # func is used for randomly generate individual with different range 
 # of parameters: 
 # L: the length of the neighborhood along the y-axis to smooth noise (1-15)
-# sigma: the standard deviation of Gaussian fun
+# sigma: the standard deviation of Gaussian function
+# w: the k
