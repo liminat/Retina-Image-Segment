ebe0b3cf9e3a56b5b28546a0a121420ff3a8ce27
liminat <180078053+liminat@users.noreply.github.com>
2023-11-30 19:47:21 +0000

    Update mod_modi.js

--- ga.py
+++ ga.py
@@ -65,4 +65,4 @@ def evalOneMax(individual):
     evalstart = timeit.default_timer()
 
     # define a varialbe of matched filter response.
-    matched = mfr.MFR(individual[0], 
+    matched = mfr.MFR(individual[0], individual[1], individua
