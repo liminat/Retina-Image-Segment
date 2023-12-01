e665e5e69b39d4c52a9d70a900c6e99f99250121
liminat <180078053+liminat@users.noreply.github.com>
2023-12-01 19:48:08 +0000

    + ‘tidal/spicule/.#one.tidal’

--- ga.py
+++ ga.py
@@ -67,4 +67,4 @@ def evalOneMax(individual):
     # define a varialbe of matched filter response.
     matched = mfr.MFR(individual[0], individual[1], individual[2], individual[3])
 
-    # generate Gaussia
+    # generate Gaussian and first-order de
