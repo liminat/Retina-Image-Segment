4c34e29aa5d6823bf673a2961112480b35ed03a8
liminat <180078053+liminat@users.noreply.github.com>
2024-01-21 15:19:39 +0000

    Bump to version 0.2.0

--- ga.py
+++ ga.py
@@ -128,4 +128,5 @@ toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
 # operator for selecting individuals for breeding the next
 # generation: each individual of the current generation
 # is replaced by the 'fittest' (best) of three individuals
-# drawn randomly from the current gene
+# drawn randomly from the current generation.
+toolbox.regi
