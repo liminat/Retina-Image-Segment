cda238af09eb73a45a326fd209af053b6e7eaf4b
liminat <180078053+liminat@users.noreply.github.com>
2024-02-16 10:12:18 +0000

    Update nl.js

--- ga.py
+++ ga.py
@@ -170,4 +170,5 @@ def main():
         offspring = list(map(toolbox.clone, offspring))
         
         # Apply crossover and mutation on the offspring
-        for child1, child2 in zip(offspring[::2], 
+        for child1, child2 in zip(offspring[::2], offspring[1::2]):
+
