16b8b9dfa7e16e524f494c3212674ad120654516
liminat <180078053+liminat@users.noreply.github.com>
2024-03-01 21:44:43 +0000

    Cleanup scripts

--- ga.py
+++ ga.py
@@ -189,4 +189,4 @@ def main():
                 del mutant.fitness.values
     
         # Evaluate the individuals with an invalid fitness
-        invalid_ind 
+        invalid_ind = [ind for ind in 
