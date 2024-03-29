0cda6e340d1da18706b95f40ccd47fb399689733
liminat <180078053+liminat@users.noreply.github.com>
2024-03-29 13:33:49 +0000

    ReadMe Updated

--- mfr.py
+++ mfr.py
@@ -15,3 +15,30 @@
 import numpy as np
 import cv2
 from math import exp, pi, sqrt
+from numbapro import vectorize
+import sys
+
+class MFR:
+    '''
+    MFR class requests L, sigme, w, c to inicialize. 
+
+    This class is used for generate filter of Gaussian (gaussian_matched_filter_kernel) and 
+    first-order derivative of Gaussian (fdog_filter_kernel)and the filter bank of the previous 
+    filters (createMatchedFilterBank). applyFilters function used for generate the matched filter
+    response of the given filter. 
+
+    '''
+
+    def __init__(self, L, sigma, w, c):
+        if L == 0:
+            L = 1
+        if sigma == 0:
+            sigma = 1
+        if w == 0:
+            w = 1
+        self.L = L
+        self.sigma = sigma
+        self.w = w
+        self.c = c
+
+    def _filter_kernel_mf_fdog(self, t = 3, mf = True):
