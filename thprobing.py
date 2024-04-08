30de2a8f4488f65b79dfb44f65952c42e6660225
liminat <180078053+liminat@users.noreply.github.com>
2024-04-08 20:15:09 +0000

    Update smart.json

--- thprobing.py
+++ thprobing.py
@@ -7,3 +7,23 @@
 
 import numpy as np
 import cv2
+import sys
+import timeit
+import copy as cp
+import os
+
+def inbounds(shape, indices):
+    '''
+    Test if the given coordinates inside the given image. 
+
+    The first input parameter is the shape of image (height, weight) and the 
+    second parameter is the coordinates to be tested (y, x)
+
+    The function returns True if the coordinates inside the image and vice versa.
+
+    '''
+    assert len(shape) == len(indices)
+    for i, ind in enumerate(indices):
+        if ind < 0 or ind >= shape[i]:
+            return False
+    return True
